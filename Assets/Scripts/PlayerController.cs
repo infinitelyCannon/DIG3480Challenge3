88f9e3b7f5d8b410254f426e9a2ec305c43cd8d3
Dakarai Simmons <infinitelyCannon@gmail.com>
2018-11-06 21:18:12 -0500

    Completed the challenge tiers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private Rigidbody body;
+    private float nextFire;
+    private AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+        sound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            sound.Play();
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        body.velocity = movement * speed;
+
+        body.position = new Vector3(
+            Mathf.Clamp(body.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(body.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        body.rotation = Quaternion.Euler(0.0f, 0.0f, body.velocity.x * -tilt);
+    }
+}
